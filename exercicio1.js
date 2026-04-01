e14905982ae017f603560c0012ac98ff069cfebe
Pedro Rocha <84788885+pedrorochacr@users.noreply.github.com>
2026-04-01 09:20:50 -0300

    calculo força gravitacional

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,8 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+function GravitacaoUniversal(G, M1, M2, d){
+    const F = (G*M1*M2)/d;
+    return F
+    
+}
