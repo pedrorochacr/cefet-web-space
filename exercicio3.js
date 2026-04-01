0576ec894243a420c12f41ea3226eb2b7c3f0544
Pedro Rocha <84788885+pedrorochacr@users.noreply.github.com>
2026-04-01 09:52:45 -0300

    expandir e retrair

--- exercicio3.js
+++ exercicio3.js
@@ -3,4 +3,15 @@
 
 const botoes = document.querySelectorAll('.botao-expandir-retrair');
 
+botoes.forEach(function(botao) {
+    botao.addEventListener('click', function(e) {
+        const paragrafo = e.currentTarget.parentElement;
+        paragrafo.classList.toggle('expandido');
 
+        if (paragrafo.classList.contains('expandido')) {
+            e.currentTarget.textContent = '-';
+        } else {
+            e.currentTarget.textContent = '+';
+        }
+    });
+});
