69c9a0bf70f96f6152cd5e3763bee05d8533edd7
Pedro Rocha <84788885+pedrorochacr@users.noreply.github.com>
2026-04-01 09:26:49 -0300

    Inserir na página

--- exercicio1.js
+++ exercicio1.js
@@ -1,8 +1,19 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
-// Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
 
-function GravitacaoUniversal(G, M1, M2, d){
-    const F = (G*M1*M2)/d;
-    return F
-    
+function GravitacaoUniversal(G, M1, M2, d) {
+    const F = (G * M1 * M2) / (d * d);
+    return F;
 }
+
+const botaoCalcular = document.querySelector('#calcular');
+
+botaoCalcular.addEventListener('click', function() {
+    const G = document.querySelector('#constante').value;
+    const M1 = document.querySelector('#massa1').value;
+    const M2 = document.querySelector('#massa2').value;
+    const d = document.querySelector('#distancia').value;
+
+    const resultado = GravitacaoUniversal(G, M1, M2, d);
+
+    document.querySelector('#resultado').value = resultado;
+});
